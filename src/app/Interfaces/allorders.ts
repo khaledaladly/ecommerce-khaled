f0bf232b987a3a0a355ce5397366bf77aeb16463
khaledaladly <khaledaladlyy@gmail.com>
2024-11-10 03:32:54 +0200

    First commit

---
+++ src/app/Interfaces/allorders.ts
@@ -0,0 +1,10 @@
+import { ShippingAddress } from './shipping-address';
+
+export interface Allorders {
+  cartItems: any[];
+  isDelivered: boolean;
+  isPaid: boolean;
+  paymentMethodType: string;
+  shippingAddress: ShippingAddress;
+  totalOrderPrice: number;
+}
